f1d3934b003491d11154e5b150fbcaa63e14651f
solidnerd <niclas@mietz.io>
2021-03-07 15:50:40 +0000

    remove: default vars

--- examples/simple_example/variables.tf
+++ examples/simple_example/variables.tf
@@ -5,7 +5,3 @@ variable "volume_size" {}
 variable "volume_filesystem" {}
 variable "hcloud_token" {}
 
-
-ssh_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJNcwP5mhs5/F2T9GFHmg4z6E6sbOG+Ynx2iPERKeOGm"
-
-hcloud_token = "l113WfwCFwZCbVcxVsQHHuMAJINQV6K8hhyVjzOMymotxb2z1oBh6ANwheFvV2lF"
